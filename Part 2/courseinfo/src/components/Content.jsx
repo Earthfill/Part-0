f5dba6831752f4bf3f4c8c9722302931c8195b7a
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2023-04-13 18:08:53 +0100

    part 2

---
+++ Part 2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Part from './Part'
+
+const Content = ({ parts }) => {
+  return (
+    <>
+    	<Part
+      	part={parts} 
+    	/>
+    </>
+  )
+}
+
+export default Content
